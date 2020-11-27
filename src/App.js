977b69eb4667c66a8457b937d0ae4023b4359dc4
David Chavarría <dabbidchavarria@gmail.com>
2020-11-27 11:51:14 -0600

    refactor: Set up react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Route } from 'react-router-dom';
+
 import InitialPage from './pages/initialPage/initialPage.component';
+import SignUpPage from './pages/signUp/signUpPage.component';
+
+import Header from './components/header/header.component';
 
 function App() {
   return (
-    <div>
-      <InitialPage />
-    </div>
+    <>
+      <Header />
+      <Route exact path='/' component={InitialPage}></Route>
+      <Route path='/signup' component={SignUpPage}></Route>
+    </>
   );
 }
 
