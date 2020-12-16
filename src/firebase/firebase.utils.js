370fa1c3e13e5ea1a58c856b6ca81d088ca644c0
David Chavarría <dabbidchavarria@gmail.com>
2020-12-15 20:10:14 -0600

    build: Set firebase configuration up

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyDiZGsWVFUu8xBNIVDIaG2ToAaVALpyrco",
+    authDomain: "food-on-the-highway.firebaseapp.com",
+    projectId: "food-on-the-highway",
+    storageBucket: "food-on-the-highway.appspot.com",
+    messagingSenderId: "789086963031",
+    appId: "1:789086963031:web:2b53759c7a6965f5962a12"
+  };
+
+firebase.initializeApp(config);
