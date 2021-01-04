0184440e1c6ca51d372aa04eb7679d81699807c3
David Chavarría <dabbidchavarria@gmail.com>
2021-01-04 02:38:23 -0600

    feat(modal): Add new modal

---
+++ src/components/modal/modal.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+const WarningModal = ({ title, body, show, ...buttonFunc }) => {
+
+    const {acceptFunc: accept, cancelFunc: cancel} = buttonFunc;
+    console.log(cancel);
+    console.log(buttonFunc);
+
+    return (
+        <Modal
+            show={show}
+            onHide={cancel}
+            backdrop='static'
+            keyboard={false}
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title>{title}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>{body}</Modal.Body>
+            <Modal.Footer>
+                <Button variant='secondary' onClick={cancel}>Close</Button>
+                <Button variant='primary' onClick={accept}>Understood</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
+
+export default WarningModal;
