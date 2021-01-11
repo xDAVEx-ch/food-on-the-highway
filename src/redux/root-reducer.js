5c7a3c3218c47230ad624c67aae200b7d1dc889b
David Chavarría <dabbidchavarria@gmail.com>
2021-01-11 01:32:25 -0600

    refactor(root-reducer): Add position reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import userReducer from './user/userReducer';
 import sidebarReducer from './sidebar/sidebar.reducer';
+import positionsReducer from './positions/positions.reducer';
 
 export default combineReducers({
     user: userReducer,
-    sidebar: sidebarReducer
+    sidebar: sidebarReducer,
+    positions: positionsReducer
 });
