5d922c92d4871e831dcb0598c55f18d97e8f1851
David Chavarría <dabbidchavarria@gmail.com>
2021-01-14 02:13:52 -0600

    refactor(positions reducer): Set initial state

--- src/redux/positions/positions.reducer.js
+++ src/redux/positions/positions.reducer.js
@@ -1,7 +1,20 @@
 import positionsActionTypes from './positions.types';
 
 const INITIAL_STATE = {
-    1: 'available'
+    1: {},
+    2: {state: 'occupied', selectedByUser: false},
+    3: {},
+    4: {},
+    5: {},
+    6: {},
+    7: {},
+    8: {},
+    9: {},
+    10: {},
+    11: {},
+    12: {state: 'occupied', selectedByUser: false},
+    13: {state: 'occupied', selectedByUser: false},
+    14: {}
 };
 
 const positionsReducer = (state = INITIAL_STATE, action) =>{
