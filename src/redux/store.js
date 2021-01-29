857e7b2694423ff048cc6f33418c3e2410121f67
David Chavarría <dabbidchavarria@gmail.com>
2021-01-29 04:04:39 -0600

    docs(store): Add configuration variable for Redux extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 
@@ -13,7 +13,9 @@ if(process.env.NODE_ENV === 'development'){
     middlewares.push(logger);
 }
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middlewares)));
 
 sagaMiddleware.run(rootSaga);
 
